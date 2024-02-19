ae015ff9100e4625c5f8d01d170522139bd309e4
Jason <mjsanchez@shaw.ca>
2024-02-19 12:29:00 -0700

    Initial commit

---
+++ src/test/java/ca/mjsanchez/ordersystem/OrdersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mjsanchez.ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
